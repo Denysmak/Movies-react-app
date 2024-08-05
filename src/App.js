43468cc5140e888fb38d701860acb529b9b91786
Denys Makene <denysmakene13@gmail.com>
2024-08-05 20:09:28 -0300

    adiciona https no lugar do http

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from './MovieCard';
 
 import './App.css';
 import SearchIcon from './search.svg'
-const API_URL = 'http://www.omdbapi.com?apikey=f4b96c5d';
+const API_URL = 'https://www.omdbapi.com?apikey=f4b96c5d';
 
 const movie1 = {
     "Title": "Blade Runner 2049",
